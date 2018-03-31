d02a348d64ae96eb7f6269edab2ee24a4a32e754
Marc Affolter <Marc Affolter@Deep-Fritz>
2018-03-31 15:36:58 +0200

    Lektion 13, Working, hidden and redirectoring

---
+++ HelloWorldServlet/src/ais/helloworldservlet/servlets/ResultsServlet.java
@@ -0,0 +1,42 @@
+package ais.helloworldservlet.servlets;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ResultServlet
+ */
+@WebServlet(name = "ResultsServlet", urlPatterns = { "/hiddenservlets/ResultsServlet.do" })
+public class ResultsServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ResultsServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");	// sage dem response Objekt was für ein Text es erwarten kann (auch PDF, mp3 was auch immer möglich)
+		PrintWriter out = response.getWriter();
+		out.println("<html><body><h1>Results are received</h1></body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
